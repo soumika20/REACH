2dd619ce77d75fe5d6c7e54bda885953e8ef5955
Karthiga2805 <140970363+Karthiga2805@users.noreply.github.com>
2025-12-04 14:11:49 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -72,6 +72,7 @@ L.Icon.Default.mergeOptions({
 
 const GOOGLE_MAPS_API_KEY = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
 const WEATHER_API_KEY = process.env.NEXT_PUBLIC_WEATHER_API_KEY;
+const GEOAPIFY_API_KEY = process.env.NEXT_PUBLIC_GEOAPIFY_API_KEY;
 
 const decodePolyline = (encoded) => {
   const poly = [];
